106bac31d1b21c995621c29d36bd10291ce670cc
Cyb10101 <cyb10101@gmail.com>
2020-02-14 20:04:26 +0100

    Time and not time

--- Programming/Binary/go-lang/macro/macro-google-remove.go
+++ Programming/Binary/go-lang/macro/macro-google-remove.go
@@ -10,6 +10,10 @@ import (
   "github.com/go-vgo/robotgo/clipboard"
 )
 
+func getUnixMilliSeconds() int64 {
+  return (time.Now().UnixNano() / 1000 / 1000);
+}
+
 type coordinates struct {
   x int
   y int
@@ -48,6 +52,8 @@ func main() {
   // Fix: Invalid MIT-MAGIC-COOKIE-1 key
 	robotgo.SetXDisplayName(os.Getenv("DISPLAY"))
 
+  var timeStart int64
+  var timeDiff int64
   var verbose bool
   var repeat uint
   var instances uint
@@ -62,81 +68,156 @@ func main() {
   robotgo.SetKeyDelay(100)
   robotgo.SetMouseDelay(100)
 
+  // Editor [text]
+  coordsEditor := coordinates {x: 2771, y: 275}
+
+  // Firefox tab
+  coordsFirefoxTab := []coordinates {
+    {x: 111, y: 46},
+    {x: 338, y: 46},
+    {x: 575, y: 46},
+    {x: 805, y: 46},
+    {x: 1001, y: 46},
+    {x: 1238, y: 46},
+    {x: 1460, y: 46},
+    {x: 1689, y: 46},
+  }
+
   // Firefox address bar
-  firefoxAddressBar := []coordinates {
-    {x: 303, y: 80},
-    {x: 1258, y: 83},
+  coordsFirefoxAddressBar := []coordinates {
+    {x: 900, y: 80},
+    {x: 900, y: 80},
+    {x: 900, y: 80},
+    {x: 900, y: 80},
+    {x: 900, y: 80},
+    {x: 900, y: 80},
+    {x: 900, y: 80},
+    {x: 900, y: 80},
   }
 
   // Search console: Remove url field
-  searchConsoleUrl := []coordinates {
-    {x: 587, y: 533},
-    {x: 1542, y: 533},
+  coordsSearchConsoleUrl := []coordinates {
+    {x: 547, y: 533},
+    {x: 547, y: 533},
+    {x: 547, y: 533},
+    {x: 547, y: 533},
+    {x: 547, y: 533},
+    {x: 547, y: 533},
+    {x: 547, y: 533},
+    {x: 547, y: 533},
   }
 
   // Search console: Entfernung beantragen
-  searchConsoleButton1 := []coordinates {
-    {x: 742, y: 533},
-    {x: 1697, y: 533},
+  coordsSearchConsoleButton1 := []coordinates {
+    {x: 740, y: 533},
+    {x: 740, y: 533},
+    {x: 740, y: 533},
+    {x: 740, y: 533},
+    {x: 740, y: 533},
+    {x: 740, y: 533},
+    {x: 740, y: 533},
+    {x: 740, y: 533},
   }
 
   // Search console: Entfernung beantragen (Bestätigung)
-  searchConsoleButton2 := []coordinates {
-    {x: 294, y: 687},
-    {x: 1251, y: 681},
+  coordsSearchConsoleButton2 := []coordinates {
+    {x: 775, y: 685},
+    {x: 775, y: 685},
+    {x: 775, y: 685},
+    {x: 775, y: 685},
+    {x: 775, y: 685},
+    {x: 775, y: 685},
+    {x: 775, y: 685},
+    {x: 775, y: 685},
   }
+
+  // Terminal
+  coordsTerminal := coordinates {x: 2066, y: 642}
 
   for i := 0; i < int(repeat); i++ {
     fmt.Printf("\nRunning query %d of %d:\n", i + 1, repeat)
 
     // Firefox: Click address bar and run url
+    timeStart = getUnixMilliSeconds()
     writeClipboard("https://www.google.com/webmasters/tools/removals")
     for i := 0; i < int(instances); i++ {
-      robotgo.MoveMouse(firefoxAddressBar[i].x, firefoxAddressBar[i].y)
+      robotgo.MoveMouse(coordsFirefoxTab[i].x, coordsFirefoxTab[i].y)
+      robotgo.MouseClick()
+      sleepMilliSeconds(200)
+
+      robotgo.MoveMouse(coordsFirefoxAddressBar[i].x, coordsFirefoxAddressBar[i].y)
       robotgo.MouseClick()
       robotgo.KeyTap("a", "ctrl")
       robotgo.KeyTap("v", "ctrl")
       robotgo.KeyTap("enter")
     }
-    fmt.Println("Wait 3 seconds: Page reload")
-    sleepSeconds(3)
 
+    timeDiff = 3000 - (getUnixMilliSeconds() - timeStart)
+    if (timeDiff > 0) {
+      fmt.Printf("Wait %d Milliseconds: Page reload\n", timeDiff)
+      sleepMilliSeconds(uint(timeDiff))
+    }
+
+    timeStart = getUnixMilliSeconds()
     for i := 0; i < int(instances); i++ {
       // Editor: Select text, cut it to clipboard and remove empty line
-      robotgo.MoveMouse(2404, 278)
+      robotgo.MoveMouse(coordsEditor.x, coordsEditor.y)
       robotgo.MouseClick()
+      sleepMilliSeconds(200)
+
       robotgo.KeyTap("home")
-      sleepMilliSeconds(100)
+      sleepMilliSeconds(200)
       robotgo.KeyTap("end", "shift")
       sleepMilliSeconds(100)
       robotgo.KeyTap("x", "ctrl")
+      sleepMilliSeconds(100)
       robotgo.KeyTap("delete")
       if (verbose) {
-        fmt.Printf("Url: %s\n", readClipboard())
+        fmt.Printf("Url %d: %s\n", (i + 1), readClipboard())
       }
 
       // Search console: Click on remove url field and paste from clipboard
-      robotgo.MoveMouse(searchConsoleUrl[i].x, searchConsoleUrl[i].y)
+      robotgo.MoveMouse(coordsFirefoxTab[i].x, coordsFirefoxTab[i].y)
       robotgo.MouseClick()
+      sleepMilliSeconds(200)
+
+      robotgo.MoveMouse(coordsSearchConsoleUrl[i].x, coordsSearchConsoleUrl[i].y)
+      robotgo.MouseClick()
+      sleepMilliSeconds(100)
       robotgo.KeyTap("v", "ctrl")
+      sleepMilliSeconds(100)
 
       // Search console: Entfernung beantragen
-      robotgo.MoveMouse(searchConsoleButton1[i].x, searchConsoleButton1[i].y)
+      robotgo.MoveMouse(coordsSearchConsoleButton1[i].x, coordsSearchConsoleButton1[i].y)
       robotgo.MouseClick()
     }
-    fmt.Println("Wait 3 seconds: Confirm removal")
-    sleepSeconds(3)
 
+    timeDiff = 4000 - (getUnixMilliSeconds() - timeStart)
+    if (timeDiff > 0) {
+      fmt.Printf("Wait %d Milliseconds: Confirm removal\n", timeDiff)
+      sleepMilliSeconds(uint(timeDiff))
+    }
+
+    timeStart = getUnixMilliSeconds()
     for i := 0; i < int(instances); i++ {
       // Search console: Entfernung beantragen (Bestätigung)
-      robotgo.MoveMouse(searchConsoleButton2[i].x, searchConsoleButton2[i].y)
+      robotgo.MoveMouse(coordsFirefoxTab[i].x, coordsFirefoxTab[i].y)
+      robotgo.MouseClick()
+      sleepMilliSeconds(200)
+
+      robotgo.MoveMouse(coordsSearchConsoleButton2[i].x, coordsSearchConsoleButton2[i].y)
       robotgo.MouseClick()
     }
-    fmt.Println("Wait 1 seconds: Removal confirmed")
+
     sleepSeconds(1)
+    timeDiff = 1000 - (getUnixMilliSeconds() - timeStart)
+    if (timeDiff > 0) {
+      fmt.Printf("Wait %d Milliseconds: Removal confirmed\n", timeDiff)
+      sleepMilliSeconds(uint(timeDiff))
+    }
   }
 
   // Terminal: Click
-  robotgo.MoveMouse(3793, 999)
+  robotgo.MoveMouse(coordsTerminal.x, coordsTerminal.y)
   robotgo.MouseClick()
 }
