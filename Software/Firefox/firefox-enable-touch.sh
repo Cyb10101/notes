1da332e54ca20af72c9827787c3ab80f07185560
Cyb10101 <cyb10101@gmail.com>
2020-07-04 12:44:31 +0200

    Firefox: Enable touch

--- Software/Firefox/firefox-enable-touch.sh
+++ Software/Firefox/firefox-enable-touch.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-sudo sed -i "s|Exec=|Exec=env MOZ_USE_XINPUT2=1 |g" /usr/share/applications/firefox.desktop
+
+# Create a user shortcut
+cp /usr/share/applications/firefox.desktop ~/.local/share/applications/firefox.desktop
+sed -i "s|Exec=|Exec=env MOZ_USE_XINPUT2=1 |g" ~/.local/share/applications/firefox.desktop
+
+# Create a global shortcut (Changes on every update)
+# sudo sed -i "s|Exec=|Exec=env MOZ_USE_XINPUT2=1 |g" /usr/share/applications/firefox.desktop
