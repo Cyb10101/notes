93118b48416d561a32d472f0dbc5dd758b19cf86
Cyb10101 <cyb10101@gmail.com>
2020-10-12 22:28:48 +0200

    Ajust archive

--- Server/Docker/webserver/mail/.docker/mail/cron/sa-learn.sh
+++ Server/Docker/webserver/mail/.docker/mail/cron/sa-learn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sa-learn --no-sync --spam /var/mail/*/*/.Junk --dbpath /var/mail-state/lib-amavis/.spamassassin
-sa-learn --no-sync --ham /var/mail/*/*/.Archive* --dbpath /var/mail-state/lib-amavis/.spamassassin
+sa-learn --no-sync --ham /var/mail/*/*/.Archiv* --dbpath /var/mail-state/lib-amavis/.spamassassin
 sa-learn --no-sync --ham /var/mail/*/*/cur* --dbpath /var/mail-state/lib-amavis/.spamassassin
 sa-learn --sync
