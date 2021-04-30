b0c38d4e3aae2835f2f904d3ddcd90cc890c0cdc
Cyb10101 <cyb10101@gmail.com>
2021-04-30 15:09:09 +0200

    Create temp directory

--- System/Linux/Scripts/create-temp.sh
+++ System/Linux/Scripts/create-temp.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-VAR_DATE=`date +%F`
-VAR_TIME=`date +%H-%M-%S`
-
 # Variant 1 - Date and time
 createTempDateTime() {
-	VAR_FOLDER='/tmp/'${1}'_'${VAR_DATE}'_'${VAR_TIME}
-	mkdir -p ${VAR_FOLDER}
+  VAR_DATE=`date +%F`
+  VAR_TIME=`date +%H-%M-%S`
+  VAR_FOLDER='/tmp/'${1}'_'${VAR_DATE}'_'${VAR_TIME}
+  mkdir -p ${VAR_FOLDER}
 }
 
 # Variant 2 - Generated
 createTempGenerated() {
-	VAR_FOLDER=`mktemp -d /tmp/${1}_XXXXXXXX`
+  VAR_TIME=`date +%H-%M`
+  VAR_FOLDER=`mktemp -d /tmp/${1}_${VAR_TIME}_XXXXXXXX`
 }
 
 #createTempDateTime 'cyb'
 createTempGenerated 'cyb'
 
 chmod og+r ${VAR_FOLDER}
-nautilus ${VAR_FOLDER} &
+xdg-open ${VAR_FOLDER} &
 exit 0;
