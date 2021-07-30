f50a779947fd13f5b9d36a2f53f5d8ef2f484518
Cyb10101 <cyb10101@gmail.com>
2021-07-31 00:57:36 +0200

    Create Ram or Tmp

--- System/Linux/Scripts/create-ramdisk.sh
+++ System/Linux/Scripts/create-ramdisk.sh
@@ -7,10 +7,16 @@ set -e
 
 ramDisk='/mnt/ramdisk'
 
+createTempGenerated() {
+  VAR_TIME=`date +%H-%M`
+  ramDisk=`mktemp -d /tmp/${1}_${VAR_TIME}_XXXXXXXX`
+  chmod og+r ${ramDisk}
+}
+
 guiSudo() {
-	#command="${@:1}"
-	#pkexec env DISPLAY=${DISPLAY} XAUTHORITY=${XAUTHORITY} sh -c '"'${command}'"'
-	pkexec env DISPLAY=${DISPLAY} XAUTHORITY=${XAUTHORITY} sh -c "${@:1}"
+  #command="${@:1}"
+  #pkexec env DISPLAY=${DISPLAY} XAUTHORITY=${XAUTHORITY} sh -c '"'${command}'"'
+  pkexec env DISPLAY=${DISPLAY} XAUTHORITY=${XAUTHORITY} sh -c "${@:1}"
 }
 
 checkGdialogInstalled() {
@@ -19,49 +25,55 @@ checkGdialogInstalled() {
     guiSudo "apt install dialog"
   fi
   if ! hash gdialog 2>/dev/null; then
-		gdialog --title "RAM Disk" --width=200 --msgbox "GDialog is not installed. Aborting!";
+    gdialog --title "RAM Disk" --width=200 --msgbox "GDialog is not installed. Aborting!";
     exit 1;
   fi
 }
 
 isRamDiskMounted() {
-	if grep -qs ${ramDisk} /proc/mounts; then
-		return 0; # true
-	else
-		return 1; # false
-	fi
+  if grep -qs ${ramDisk} /proc/mounts; then
+    return 0; # true
+  else
+    return 1; # false
+  fi
 }
 
 checkIfNotMounted() {
-	if isRamDiskMounted; then
-		if zenity --title "RAM Disk" --width=210 --question --text="RAM Disk is already mounted!\nUnmount current RAM Disk?"; then
-			guiSudo "umount ${ramDisk}"
-			checkIfNotMounted
-		else
-			exit 1;
-		fi
-	fi
+  if isRamDiskMounted; then
+    if zenity --title "RAM Disk" --width=210 --question --text="RAM Disk is already mounted!\nUnmount current RAM Disk?"; then
+      guiSudo "umount ${ramDisk}"
+      checkIfNotMounted
+    else
+      exit 1;
+    fi
+  fi
 }
 
 createRamDisk() {
-	# Duplicate file descriptor 1 on descriptor 3
-	exec 3>&1
+  # Duplicate file descriptor 1 on descriptor 3
+  exec 3>&1
 
-	# Generate the dialog box
-	set +e
-	echo 1
-	ramSize=$(gdialog --title "RAM Disk" --width=200 --inputbox "How much GB?" 2>&1 1>&3)
-	set -e
+  # Generate the dialog box
+  set +e
+  echo 1
+  ramSize=$(gdialog --title "RAM Disk" --width=200 --inputbox "How much GB?" 2>&1 1>&3)
+  set -e
 
-	# Close file descriptor 3
-	exec 3>&-
+  # Close file descriptor 3
+  exec 3>&-
 
-	# Act on the exit status
-	if [[ $ramSize =~ ^-?[0-9]+$ ]] && [[ $ramSize > 0 ]]; then
-		guiSudo "mkdir -p ${ramDisk} && mount -t tmpfs -o rw,size=${ramSize}G tmpfs ${ramDisk}"
-	fi
+  # Act on the exit status
+  if [[ $ramSize =~ ^-?[0-9]+$ ]] && [[ $ramSize > 0 ]]; then
+    guiSudo "mkdir -p ${ramDisk} && mount -t tmpfs -o rw,size=${ramSize}G tmpfs ${ramDisk}"
+  fi
 }
 
+#checkGdialogInstalled
+#checkIfNotMounted
+#createRamDisk
+#xdg-open ${ramDisk} &
+
 checkGdialogInstalled
-checkIfNotMounted
+createTempGenerated 'ram'
 createRamDisk
+xdg-open ${ramDisk} &
