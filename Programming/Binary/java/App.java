06c311fcc2a2d72d0a765f1ca73b24ed29540b26
Cyb10101 <cyb10101@gmail.com>
2022-08-27 21:53:17 +0200

    Java example

---
+++ Programming/Binary/java/App.java
@@ -0,0 +1,12 @@
+// javac App.java && java App
+// javac App.java && jar -c -f App.jar -e App *.class Utility/*.class && java -jar App.jar
+import Utility.Tools;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hellow World");
+
+        Tools tools = new Tools();
+        System.out.println("Result: " + tools.add(3, 5));
+    }
+}
---
+++ Programming/Binary/java/Utility/Tools.java
@@ -0,0 +1,7 @@
+package Utility;
+
+public class Tools {
+    public int add(int x, int y) {
+        return (x + y);
+    }
+}
