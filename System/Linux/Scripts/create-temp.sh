b79066dec7c943666c68bb679a858c4ea300531b
Cyb10101 <cyb10101@gmail.com>
2023-06-30 14:51:36 +0200

    Create temp

--- System/Linux/Scripts/create-temp.sh
+++ System/Linux/Scripts/create-temp.sh
@@ -4,11 +4,25 @@
 # test -f ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs && source ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs
 # ln -s /usr/local/bin/create-temp ${XDG_DESKTOP_DIR:-${HOME}/Desktop}/create-temp
 
+createLauncher() {
+cat <<EOF | sudo tee /usr/share/applications/create-temp.desktop
+[Desktop Entry]
+Name=Temporary Directory
+Comment=Create a temporary Directory
+Exec="/usr/local/bin/create-temp"
+TryExec=/usr/local/bin/create-temp
+Terminal=false
+Type=Application
+Icon=/usr/share/icons/Yaru-blue/256x256/actions/folder-new.png
+#Icon=/usr/share/icons/elementary-xfce/actions/128/folder-new.png
+Categories=Utility;
+EOF
+}
+
 # Variant 1 - Date and time
 createTempDateTime() {
-  VAR_DATE=`date +%F`
-  VAR_TIME=`date +%H-%M-%S`
-  VAR_FOLDER='/tmp/'${1}'_'${VAR_DATE}'_'${VAR_TIME}
+  VAR_DATE=`date +%Y-%m-%d_%H-%M-%S`
+  VAR_FOLDER=`/tmp/${1}_${VAR_DATE}`
   mkdir -p ${VAR_FOLDER}
   chmod 750 ${VAR_FOLDER}
 }
