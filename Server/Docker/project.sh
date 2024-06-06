faf7bdd64f6186b2914741613d31420312e628c5
Cyb10101 <cyb10101@gmail.com>
2024-06-06 13:59:00 +0200

    project.sh updated

--- Server/Docker/project.sh
+++ Server/Docker/project.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# docker image prune -a
+# docker ps --filter "status=exited" --format "table {{.ID}}\t{{.Image}}\t{{.Status}}"
+
+# nextcloud
+# vim docker-compose.yml
+# grep 'version' /root/projects/nextcloud/.docker/nextcloud/config/config.php
+
+# Useful for script
+scriptPath="$(cd "$(dirname "${0}")" >/dev/null 2>&1; pwd -P)"
+
+textColor() {
+  echo -e "\033[0;3${1}m${2}\033[0m"
+}
+
 checkDialogInstalled() {
   if ! hash dialog 2>/dev/null; then
     echo "Dialog is not installed!";
@@ -8,6 +22,43 @@ checkDialogInstalled() {
   if ! hash dialog 2>/dev/null; then
     echo "Dialog is not installed. Aborting!";
     exit 1;
+  fi
+}
+
+# Script #######################################################################
+getDirectories() {
+  directories=(
+    global
+    mail
+    nextcloud
+
+    $(ls -d */ | sed 's/\/$//' | grep -E '^\.\/user_.*$' | sort)
+    $(ls -d */ | sed 's/\/$//' | grep -Ev '^\.\/(global|mail|nextcloud|user_.*)$' | sort)
+  )
+  echo "${directories[@]}"
+}
+
+eachDirectory() {
+  local command=${1}
+  shift
+  local directories=("$@")
+  for directory in "${directories[@]}"; do
+    if [ -d "${directory}" ]; then
+      textColor 2 "# ${directory}"
+      cd "${directory}"
+      eval ${command}
+      cd ..
+    else
+      textColor 1 "# Error: Directory '${directory}' not found!"
+    fi
+    done
+}
+
+startOrDockerCompose() {
+  if [ -f "start.sh" ]; then
+    ./start.sh "$@"
+  else
+    docker-compose "$@"
   fi
 }
 
@@ -21,125 +72,115 @@ checkGitMaster() {
 
 checkGit() {
     if [[ $(git diff --stat) != '' ]]; then
-        echo
-        git status --porcelain
-        echo
+        echo; git status --short; echo
         printf "Git is dirty...\n";
         return 1;
     fi
     return 0;
 }
 
-printDirectory() {
-    printf "\e[1;34m\n%-6s\e[m\n" "$(dirname ${1})";
-}
-
 menu() {
   selected=`dialog --clear --backtitle "Projects" --no-tags --menu "Menu" 0 0 0 \
-    "dc-down"         "Docker: Shutdown all projects" \
-    "git-dirty"       "Git: Show dirty git" \
-    "dc-pull"         "Docker: Download all images from projects" \
-    "dc-upgrade"      "Docker: Upgrade every docker container" \
-    "dc-up"           "Docker: Start all projects" \
-    "dc-pull-images"  "Docker: Download all docker images" \
-    "git-pull"        "Git: Pull all git" \
-    "sync"            "Sync: Syncronize project folder" \
+    "projectStatus"           "Show docker status" \
+    "projectPull"             "Pull docker images" \
+    "projectStart"            "Start server (start: pull, build, up)" \
+    "projectUp"               "Start server (up)" \
+    "projectDown"             "Stop server" \
+    "projectRestart"          "Restart server (restart)" \
+    "projectRestartUpDown"    "Restart server (down + up)" \
+    "projectGitDirty"         "Git: Show if git is dirty" \
+    "projectWebsiteUrls"      "Get website urls" \
    3>&1 1>&2 2>&3`
   clear
   echo ""
   if [[ "${selected}" == "" ]]; then
     exit 0
   fi
-  ${0} ${selected}
+  ${selected}
 }
 
-syncProjects() {
-  sshList=( $(cat ~/.ssh/config | grep Host | grep -v Hostname | grep cyb- | sed 's/Host //g' | sed 's/ /\n/g') )
+projectStatus() {
+  directories=( $(getDirectories) )
+  eachDirectory 'startOrDockerCompose ps' "${directories[@]}"
+}
 
-  serverList=""
-  for item in "${sshList[@]}"; do
-     serverList="${serverList}\"${item}\" \"${item}\" ";
-  done
+projectPull() {
+  directories=( $(getDirectories) )
+  eachDirectory 'startOrDockerCompose pull' "${directories[@]}"
+}
 
-  selectedServer=`dialog --clear --backtitle "Projects" --no-tags --menu "Sync to SSH Server" 0 0 0 \
-  $(echo $serverList | tr -d '"') \
-   3>&1 1>&2 2>&3`
-  dialog --clear --backtitle "Projects syncronisation" --title "" --yesno "Really sync to Server: ${selectedServer}\n\nWARNING: All Docker should be down!" 0 0
-  response=$?
-  clear
-  echo ""
-  if [ ${response} == 0 ]; then
-    echo "Stop Docker 'global'..."
-    docker-compose -f ~/projects/global/docker-compose.yml down --remove-orphans
-    if [ ! -f global.tar.xz ]; then
-      echo "Create archiv 'global'..."
-      sudo tar cfJ global.tar.xz global
-      sudo chown ${USER}:${USER} global.tar.xz
+projectStart() {
+  directories=( $(getDirectories) )
+  eachDirectory 'startOrDockerCompose start' "${directories[@]}"
+}
+
+projectUp() {
+  directories=( $(getDirectories) )
+  eachDirectory 'startOrDockerCompose up' "${directories[@]}"
+}
+
+projectDown() {
+  directories=( $(getDirectories) )
+  directories=( $(printf '%s\n' "${directories[@]}" | tac | tr '\n' ' '; echo) ) # Reverse array
+  eachDirectory 'startOrDockerCompose down' "${directories[@]}"
+}
+
+projectRestart() {
+  directories=( $(getDirectories) )
+  eachDirectory 'startOrDockerCompose restart' "${directories[@]}"
+}
+
+projectRestartUpDown() {
+  directories=( $(getDirectories) )
+  eachDirectory 'startOrDockerCompose down && startOrDockerCompose up' "${directories[@]}"
+}
+
+projectGitDirty() {
+  directories=( $(getDirectories) )
+  eachDirectory 'checkGitMaster && checkGit' "${directories[@]}"
+}
+
+projectWebsiteUrls() {
+  directories=( $(getDirectories) )
+  eachDirectory 'projectWebsiteUrlsCommand' "${directories[@]}"
+}
+projectWebsiteUrlsCommand() {
+  local developmentSuffix=''
+  #if [ "$(isContextDevelopment)" == "1" ]; then
+  #  developmentSuffix='.dev'
+  #fi
+
+  DOCKER_COMPOSE_FILE="compose${developmentSuffix}.yaml"
+  if [ -e "compose${developmentSuffix}.yml" ]; then
+    DOCKER_COMPOSE_FILE="compose${developmentSuffix}.yml"
+  elif [ -e "docker-compose${developmentSuffix}.yaml" ]; then
+    DOCKER_COMPOSE_FILE="docker-compose${developmentSuffix}.yaml"
+  elif [ -e "docker-compose${developmentSuffix}.yml" ]; then
+    DOCKER_COMPOSE_FILE="docker-compose${developmentSuffix}.yml"
+  fi
+
+  if [ -e "${DOCKER_COMPOSE_FILE}" ]; then
+    local traefikHosts=$(yq -r '.services[].labels | select(. != null) | .[] | split("=") | select(.[0] | match("traefik\\.http\\.routers\\..*\\.rule")) | .[1]' "${DOCKER_COMPOSE_FILE}" | grep -Po '(?<=Host\(`)([^`]+)(?=`\))')
+    if [[ ! -z "${traefikHosts[@]}" ]]; then
+      IFS=' ' read -ra traefikHosts <<< "$(echo ${traefikHosts[@]})"
+    else
+      traefikHosts=()
     fi
-    echo "Start Docker 'global'..."
-    docker-compose -f ~/projects/global/docker-compose.yml up -d
 
-    echo "Remove remote 'global'..."
-    ssh ${selectedServer} docker-compose -f ~/projects/global/docker-compose.yml down --remove-orphans
-    ssh ${selectedServer} gio trash ~/projects/global
+    local virtualHosts=$(yq -r '.services[].environment | select(. != null) | .[] | split("=") | select(.[0] == "VIRTUAL_HOST") | .[1]' "${DOCKER_COMPOSE_FILE}")
+    if [ ! -z "${virtualHosts}" ]; then
+      IFS=',' read -ra virtualHosts <<< "${virtualHosts}"
+    else
+      virtualHosts=()
+    fi
 
-    echo "Sync projects..."
-    rsync -av --delete --exclude="global" \
-      --exclude="docker_backup-tools/config/ssh" \
-      ~/projects/./ ${selectedServer}:projects/
-
-    echo "Extract archiv 'global'..."
-    ssh ${selectedServer} tar xf ~/projects/global.tar.xz --directory ~/projects
-    ssh ${selectedServer} gio trash ~/projects/global.tar.xz
-    ssh ${selectedServer} docker-compose -f ~/projects/global/docker-compose.yml up -d
-    if [ -f global.tar.xz ]; then
-      rm global.tar.xz
-    fi;
+    local websites=("${traefikHosts[@]}" "${virtualHosts[@]}")
+    IFS=" " read -ra websites <<< "$(echo "${websites[@]}" | tr ' ' '\n' | sort -u | grep -vE '^www\.' | tr '\n' ' ')"
+    for website in "${websites[@]}"; do
+      echo "  - https://${website}"
+    done
   fi
 }
 
 checkDialogInstalled
-export -f checkGitMaster
-export -f checkGit
-export -f printDirectory
-
-case "${1}" in
-  dc-down)
-    find ~/projects/ -path '*docker-global' -prune -o -path '*global' -prune -o -type f -name 'docker-compose.yml' -o -type f -name 'docker-compose.yaml' -exec bash -c '(printDirectory "{}"; docker-compose -f "{}" down --remove-orphans)' \;
-  ;;
-  dc-pull)
-    find ~/projects/ -path '*docker-global' -prune -o -path '*global' -prune -o -type f -name 'docker-compose.yml' -o -type f -name 'docker-compose.yaml' -exec bash -c '(printDirectory "{}"; docker-compose -f "{}" pull)' \;
-  ;;
-  dc-upgrade)
-    find ~/projects/ -path '*docker-global' -prune -o -path '*global' -prune -o -type f -name 'docker-compose.yml' -o -type f -name 'docker-compose.yaml' -exec bash -c '(printDirectory "{}"; docker-compose -f "{}" pull && docker-compose -f "{}" down --remove-orphans && docker-compose -f "{}" build && docker-compose -f "{}" up -d && docker-compose -f "{}" down --remove-orphans)' \;
-  ;;
-  dc-up)
-    find ~/projects/ -path '*docker-global' -prune -o -path '*global' -prune -o -type f -name 'docker-compose.yml' -o -type f -name 'docker-compose.yaml' -exec bash -c '(printDirectory "{}"; docker-compose -f "{}" build; docker-compose -f "{}" up -d)' \;
-  ;;
-  dc-pull-images)
-    docker pull cyb10101/php-dev:apache-5.6
-    docker pull cyb10101/php-dev:apache-7.0
-    docker pull cyb10101/php-dev:apache-7.1
-    docker pull cyb10101/php-dev:apache-7.2
-    docker pull cyb10101/php-dev:apache-7.3
-    docker pull cyb10101/php-dev:apache-7.4
-    docker pull cyb10101/php-dev:nginx-5.6
-    docker pull cyb10101/php-dev:nginx-7.0
-    docker pull cyb10101/php-dev:nginx-7.1
-    docker pull cyb10101/php-dev:nginx-7.2
-    docker pull cyb10101/php-dev:nginx-7.3
-    docker pull cyb10101/php-dev:nginx-7.4
-  ;;
-  git-dirty)
-    find ~/projects/ -maxdepth 2 -path '*docker-global' -prune -o -path '*global' -prune -o -type d -name '.git' -exec bash -c '(printDirectory "{}"; cd "$(dirname {})"; checkGitMaster && checkGit; (cd - > /dev/null 2>&1))' \;
-  ;;
-  git-pull)
-    find ~/projects/ -maxdepth 2 -path '*docker-global' -prune -o -path '*global' -prune -o -type d -name '.git' -exec bash -c '(printDirectory "{}"; cd "$(dirname {})"; checkGitMaster && checkGit && git pull origin master; (cd - > /dev/null 2>&1))' \;
-  ;;
-  sync)
-    syncProjects
-  ;;
-  *)
-    menu
-  ;;
-esac
+menu
