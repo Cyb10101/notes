fb373f7f30973b6af288a0ea7b0bcd27bc8a40e5
Cyb10101 <cyb10101@gmail.com>
2025-04-02 17:14:12 +0200

    Disable suspend audio, audio sample rate

---
+++ System/Linux/Configuration/Audio/wireplumber_disable-session-suspend.lua
@@ -0,0 +1,31 @@
+-- Configure audio suspend
+-- If your sound don't hear notifications and your videos start delayed, your audio card is suspended
+-- Filename: ~/.config/wireplumber/main.lua.d/disable-session-suspend.lua
+-- ln -sr ~/Sync/notes/System/Linux/Configuration/Audio/wireplumber_disable-session-suspend.lua ~/.config/wireplumber/main.lua.d/disable-session-suspend.lua
+
+-- Restart for changes to take effect
+-- systemctl --user restart pipewire wireplumber
+
+-- Show if audio device suspended
+-- sudo apt install pulseaudio-utils
+-- watch -c -n 0.1 "pactl list short sinks"
+-- watch -c -n 0.1 "pactl list short sinks | sed -E -e 's/(SUSPENDED)/\x1b[31m\1\x1b[0m/g' -e 's/(IDLE)/\x1b[33m\1\x1b[0m/g' -e 's/(RUNNING)/\x1b[32m\1\x1b[0m/g'"
+
+table.insert (alsa_monitor.rules, {
+    matches = {
+      {
+        -- Matches all sources.
+        { "node.name", "matches", "alsa_input.*" },
+      },
+      {
+        -- Matches all sinks.
+        { "node.name", "matches", "alsa_output.*" },
+      },
+    },
+    apply_properties = {
+      --["session.suspend-timeout-seconds"] = 0, -- Disables suspend, on some systems this won't work and you need to set a high value instead
+      --["session.suspend-timeout-seconds"] = 86400, -- 1 Day
+      --["session.suspend-timeout-seconds"] = 3600, -- 1 Hour
+      ["session.suspend-timeout-seconds"] = 300, -- 5 Minutes
+    },
+})
