ede04253bbf7b9d8e74ef19b6c092565d11bed3a
Cyb10101 <cyb10101@gmail.com>
2025-04-02 20:08:12 +0200

    Import mok, module signing updated

--- System/Linux/Secure-Boot/Secure-Boot.md
+++ System/Linux/Secure-Boot/Secure-Boot.md
@@ -1,7 +1,18 @@
 # Linux: Secure Boot
 
+## Import machine key
+
+```bash
+# Test if key is enrolled
+sudo mokutil --test-key /var/lib/shim-signed/mok/MOK.der
+
+# Enroll key on next boot
+sudo mokutil --import /var/lib/shim-signed/mok/MOK.der
+```
+
 ## Module signing for drivers
 
+*Note: Maybe not needed again!*
 *Note: Must be run after new kernel update!*
 
 @todo [Linux Secure Boot: Module signing should go automatically](https://github.com/Cyb10101/notes/issues/1)
--- System/Linux/Secure-Boot/module-signing.sh
+++ System/Linux/Secure-Boot/module-signing.sh
@@ -1,28 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-PATH_CERTIFICATE="/root/module-signing"
-SIGN_MODULES=(
-  evdi
-  vboxdrv
-)
-
-colorRed='\033[0;31m'
-colorGreen='\033[0;32m'
-colorYellow='\033[0;33m'
-colorReset='\033[0m'
+textColor() {
+  echo -e "\033[0;3${1}m${2}\033[0m"
+}
 
 checkUserRoot() {
   if [[ $EUID -ne 0 ]]; then
-    echo -e "${colorRed}You must be a root user!${colorReset}"
+    textColor 1 "You must be a root user!"
     exit 1
   fi
 }
 
 checkMokUtil() {
   if ! hash mokutil 2>/dev/null; then
-    echo -e "${colorRed}[ERROR] MOK utility not installed!${colorReset}"
-    echo -e "${colorYellow}Try: apt install mokutil${colorReset}"
+    textColor 1 "[ERROR] MOK utility not installed!"
+    textColor 3 "Try: apt install mokutil"
     sleep 3;
     exit 1;
   fi
@@ -31,7 +24,7 @@ checkMokUtil() {
 checkSecureBootEnabled() {
   SECUREBOOT_STATE=$(mokutil --sb-state | tr '[:upper:]' '[:lower:]' | head -n 1)
   if [ "${SECUREBOOT_STATE}" != "secureboot enabled" ]; then
-    echo -e "${colorRed}[ERROR] Secure Boot is currently not enabled:${colorReset}"
+    textColor 1 "[ERROR] Secure Boot is currently not enabled:"
     mokutil --sb-state
     sleep 3;
     exit 1;
@@ -39,49 +32,66 @@ checkSecureBootEnabled() {
 }
 
 createFolder() {
-	if [ ! -d "${1}" ]; then
-		mkdir -p "${1}";
-	fi;
-	if [ ! -d "${1}" ]; then
-		echo -e "${colorRed}Can not create folder '${1}'${colorReset}";
-		exit 1;
-	fi;
+  if [ ! -d "${1}" ]; then
+    mkdir -p "${1}";
+  fi;
+  if [ ! -d "${1}" ]; then
+    textColor 1 "Can not create folder '${1}'";
+    exit 1;
+  fi;
 }
 
 createCertificateAndImport() {
   # Create signing key and import to MOK
   if [ ! -f "${PATH_CERTIFICATE}/mok.priv" ]; then
-    echo -e "${colorYellow}Create new certificate for secure boot modules!${colorReset}"
+    textColor 3 "Create new certificate for secure boot modules!"
     openssl req -new -x509 -newkey rsa:2048 -keyout ${PATH_CERTIFICATE}/mok.priv -outform DER -out ${PATH_CERTIFICATE}/mok.der -nodes -days 36500 -subj "/CN=${1}/"
 
-    echo -e "\n${colorYellow}Import MOK: (Use simple password)${colorReset}"
-    mokutil --import ${PATH_CERTIFICATE}/mok.der
+    textColor 3 "\nImport MOK: (Use simple password)"
+    sudo mokutil --import ${PATH_CERTIFICATE}/mok.der
   else
-    echo -e "${colorGreen}[OK] Certificate already exists.${colorReset}"
+    textColor 2 "[OK] Certificate already exists."
   fi
 }
 
 signModules() {
-  echo -e "\n${colorYellow}Sign modules:${colorReset}"
+  textColor 3 "\nSign modules:"
+  set +e
   for module in "${SIGN_MODULES[@]}"; do
-    if modinfo -n "${module}" &> /dev/nul; then
-        echo -e "${colorGreen}* ${module} ($(modinfo -n ${module}))${colorReset}"
-        /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 ${PATH_CERTIFICATE}/mok.priv ${PATH_CERTIFICATE}/mok.der $(modinfo -n "${module}")
+    if modinfo -n "${module}" &> /dev/null; then
+      textColor 2 "* ${module} ($(modinfo -n ${module}))"
+      sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 ${PATH_CERTIFICATE}/mok.priv ${PATH_CERTIFICATE}/mok.der $(modinfo -n "${module}")
     else
-        echo -e "${colorRed}* ${module} (not found!)${colorReset}"
+      textColor 1 "* ${module} (not found!)"
     fi
   done
+  set -e
 }
 
+# Script #######################################################################
+PATH_CERTIFICATE="/root/module-signing"
+SIGN_MODULES=(
+  evdi
+  vboxdrv
+  vboxnetflt
+  vboxnetadp
+  vboxpci
+)
+#tail $(modinfo -n vboxdrv) | grep "Module signature appended"
+
 checkUserRoot
+#textColor 3 "Elevate sudo pemissions!"
+#sudo echo 'Wonderful...'
+
 checkMokUtil
 checkSecureBootEnabled
 createFolder "${PATH_CERTIFICATE}"
 createCertificateAndImport "Owner module signing"
 signModules
 
-echo -e "\n${colorYellow}Reboot and enroll MOK!${colorReset}"
-echo -e "MOK management utility will automatically start."
-echo -e "Choose: Enroll MOK"
-echo -e "Continue and confirm enrollment"
-echo -e "Use the previous password to import the certificate."
+echo ''
+textColor 3 "Reboot and enroll MOK!"
+echo "MOK management utility will automatically start."
+echo "Choose: Enroll MOK"
+echo "Continue and confirm enrollment"
+echo "Use the previous password to import the certificate."
