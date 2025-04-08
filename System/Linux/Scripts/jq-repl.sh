e31f4aeaa046e0702c246e3fa8e08ef40a7507b9
Cyb10101 <cyb10101@gmail.com>
2025-04-08 18:39:31 +0200

    jq-repl

--- System/Linux/Scripts/jq-repl.sh
+++ System/Linux/Scripts/jq-repl.sh
@@ -1,12 +1,34 @@
 #!/usr/bin/env bash
 # sudo install ~/Sync/notes/System/Linux/Scripts/jq-repl.sh /usr/local/bin/jq-repl
 
-if [[ -z ${1} ]] || [[ ${1} == '-' ]]; then
+# Tests
+# echo '[{"project": "Test 1"}, {"project": "Test 2"}]' | ~/Sync/notes/System/Linux/Scripts/jq-repl.sh
+# echo '[{"project": "Test 1"}, {"project": "Test 2"}]' | ~/Sync/notes/System/Linux/Scripts/jq-repl.sh -q '.[].project'
+# ~/Sync/notes/System/Linux/Scripts/jq-repl.sh file.json
+# ~/Sync/notes/System/Linux/Scripts/jq-repl.sh -q '.[].project' file.json
+
+args=()
+query='.'
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -q|--query)
+            query="$2"
+            shift 2
+        ;;
+        *)
+            args+=("$1")
+            shift
+        ;;
+    esac
+done
+
+if [[ ( ${#args[@]} -eq 0 || ${#args[@]} -eq 1 ) && ( -z "${args[0]}" || ${args[0]} == '-' ) ]]; then
     input=$(mktemp /tmp/jq-repl_XXXXXXXX)
     #trap 'rm -f "$input"' EXIT
     trap 'rm "$input"' EXIT
-    cat /dev/stdin > "${input}"
+    cat /dev/stdin > "$input"
 else
-    input=${1}
+    input="${args[@]}"
 fi
-echo '' | fzf --disabled --preview-window='up:90%' --query='.' --print-query --preview "jq --color-output -r {q} ${input}"
+
+echo '' | fzf --disabled --preview-window='up:90%' --query="$query" --print-query --preview "jq --color-output -r {q} ${input}"
